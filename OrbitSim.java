4c2e0cc481280e543162ad01124f91072acd0568
Cai John <caiwjohn@gmail.com>
2016-02-17 18:51:35 +0000

    start of object for time-integration

---
+++ OrbitSim.java
@@ -0,0 +1,50 @@
+/**
+ *
+ * N-body Simulation of the Solar System:
+ *
+ * This simulation utilizes the velocity-Verlet time-integration method in
+ * conjuncture with Newtonian Gravitation to simulate the orbits of 
+ * bodies in the solar system.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *@author C. John
+ *@author M. O'Shea
+ *@version "02/2016"
+ */
+
+
+import java.io.*;
+import java.util.Scanner;
+
+
+
+public static toVMD(Particle3d[] print, String file){
+
+
+
+
+}
+
+
+
+public class OrbitSim {
+
+
+      /**
+     * Main method to run OrbitSim class.
+     *
+     *@param argv[0] name of the output file for position.
+     *@param argv[1] name of the output file for the total energy
+     *@param argv[2] number of timesteps for time integration
+     *@param argv[3] size of each timestep
+     *
+x     */
+
+    // We are using file IO so we are throwing an exception
+
+    public static void main (String[] argv) throws IOException{
